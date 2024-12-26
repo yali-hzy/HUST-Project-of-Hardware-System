8e278575988d166629d826adfaed0d745e18b6b3
Zhou Luyan <luyan_zhou@outlook.com>
2024-12-26 17:09:58 +0800

    feat: impl Painter for VGA

--- magic_cube/bare_metal/src/vga/mod.rs
+++ magic_cube/bare_metal/src/vga/mod.rs
@@ -1,9 +1,17 @@
+use core::arch::asm;
+
+use game::Painter;
+
+use crate::console::{pause, print};
+
 pub const VGA_WIDTH: usize = 488;
-pub const _VGA_HEIGHT: usize = 280;
+pub const VGA_HEIGHT: usize = 280;
 
-pub struct Vga {}
+pub struct Vga {
+    color: u8,
+}
 
-pub static VGA: Vga = Vga {};
+static mut VGA: Vga = Vga { color: 0x0F };
 
 impl Vga {
     pub fn get_frame_buffer(&self) -> *mut u8 {
@@ -15,19 +23,38 @@ impl Vga {
 
     pub fn draw_pixel(&self, x: usize, y: usize, color: u8) {
         assert!(x < VGA_WIDTH);
-        assert!(y < _VGA_HEIGHT);
+        assert!(y < VGA_HEIGHT);
 
         let frame_buffer = self.get_frame_buffer();
         let offset = x + y * VGA_WIDTH;
         let bit = offset & 1;
         let old_color = unsafe { frame_buffer.add(offset >> 1).read_volatile() };
-        let color = if bit == 0 {
+        let new_color = if bit == 0 {
             (old_color & 0xf0) | color
         } else {
             (old_color & 0x0f) | (color << 4)
         };
         unsafe {
-            frame_buffer.add(offset >> 1).write_volatile(color);
+            frame_buffer.add(offset >> 1).write_volatile(new_color);
         }
     }
+}
+
+impl Painter for Vga {
+    fn draw(&mut self, x: usize, y: usize) {
+        self.draw_pixel(x, y, self.color);
+    }
+
+    fn set_color(&mut self, color: u8) {
+        self.color = color;
+    }
+}
+
+pub fn init() {
+    print(get_vga().get_frame_buffer() as usize);
+    pause();
+}
+
+pub fn get_vga() -> &'static mut Vga {
+    unsafe { (&raw mut VGA).as_mut().unwrap() }
 }
